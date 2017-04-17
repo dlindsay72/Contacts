50b9ec1e075f706e3be3473cfb52a63c1efd86b9
Dan Lindsay <dlindsay72@hotmail.com>
2017-04-17 15:56:24 -0400

    Project setup done, ContactsDB.plist added and corresponding images

---
+++ Contacts/ContactListController.swift
@@ -0,0 +1,39 @@
+//
+//  ContactListController.swift
+//  Contacts
+//
+//  Created by Dan Lindsay on 2017-04-17.
+//  Copyright © 2017 Dan Lindsay. All rights reserved.
+//
+
+import UIKit
+
+class ContactListController: UITableViewController {
+    
+    var contacts = ContactsSource.contacts
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 0
+    }
+
+   
+
+}
